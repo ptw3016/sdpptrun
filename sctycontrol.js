f42faa58f50c8ab7b1dbcf5bdaac4d26e201d10b
ptw3016 <132131274+ptw3016@users.noreply.github.com>
2023-08-21 21:11:43 +0900

    sdpptrun 36

--- sctycontrol.js
+++ sctycontrol.js
@@ -1,4 +1,4 @@
-const schedule = require('node-schedule-tz');
+const schedule = require('node-schedule');
 const { TuyaContext } = require('@tuya/tuya-connector-nodejs');
 const { OAuth2Client } = require('google-auth-library');
 var nodemailer = require('nodemailer');
@@ -65,8 +65,8 @@ async function sctytimebkPr() {
     // 월요일과 수요일을 숫자로 매핑합니다 (0: 일요일, 1: 월요일, ..., 6: 토요일).
     const targetDaysOfWeek = [0, 1, 2, 3, 4, 5, 6]; //
     const hgDayWeek = ["일", "월", "화", "수", "목", "금", "토"]
-    const hour = 20;  // 24시간 형식  
-    const minute = 56;
+    const hour = 21;// 24시간 형식  
+    const minute = 13;
 
     // 원하는 요일과 시간에 함수를 실행하도록 스케줄링합니다.
     const rule = new schedule.RecurrenceRule();
