9d5e19ad7591e0e338e64b941d7456f1f5c8f5fc
ptw3016 <132131274+ptw3016@users.noreply.github.com>
2023-08-21 20:53:21 +0900

    sdpptrun 35

--- sctycontrol.js
+++ sctycontrol.js
@@ -66,7 +66,7 @@ async function sctytimebkPr() {
     const targetDaysOfWeek = [0, 1, 2, 3, 4, 5, 6]; //
     const hgDayWeek = ["일", "월", "화", "수", "목", "금", "토"]
     const hour = 20;  // 24시간 형식  
-    const minute = 52;
+    const minute = 56;
 
     // 원하는 요일과 시간에 함수를 실행하도록 스케줄링합니다.
     const rule = new schedule.RecurrenceRule();
