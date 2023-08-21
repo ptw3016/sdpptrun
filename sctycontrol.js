f5e3b8ba14c188951a842da4036c0d6a5cd2dcb8
ptw3016 <132131274+ptw3016@users.noreply.github.com>
2023-08-21 19:38:41 +0900

    sdpptrun 31

--- sctycontrol.js
+++ sctycontrol.js
@@ -62,19 +62,18 @@ async function dolcstatechk(device_id) { //dlcs control - chk
 
 
 async function sctytimebkPr() {
-
+    const utccv1 = 9;
     // 월요일과 수요일을 숫자로 매핑합니다 (0: 일요일, 1: 월요일, ..., 6: 토요일).
     const targetDaysOfWeek = [0, 1, 2, 3, 4, 5, 6]; //
     const hgDayWeek = ["일", "월", "화", "수", "목", "금", "토"]
-    const hour = 19; // 24시간 형식
-    const minute = 33;
+    const hour = 19 - utccv1; // 24시간 형식  /utccv1은 utc 시간 매칭
+    const minute = 42;
 
     // 원하는 요일과 시간에 함수를 실행하도록 스케줄링합니다.
     const rule = new schedule.RecurrenceRule();
     rule.dayOfWeek = targetDaysOfWeek;
     rule.hour = hour;
     rule.minute = minute;
-    //rule.tz = 'Etc/UTC'; // UTC 타임존 설정 - 배포용 / 로컬에서는 이거 주석처리
 
     const job = schedule.scheduleJob(rule, sdbgdlchkPr);
 
