88cadddb6bba570fee9c5ae18efba92e0813244c
ptw3016 <132131274+ptw3016@users.noreply.github.com>
2023-08-22 12:44:40 +0900

    sdpptrun 39

--- sctycontrol.js
+++ sctycontrol.js
@@ -2,6 +2,9 @@ const schedule = require('node-schedule');
 const { TuyaContext } = require('@tuya/tuya-connector-nodejs');
 const { OAuth2Client } = require('google-auth-library');
 var nodemailer = require('nodemailer');
+const axios = require('axios');
+const crypto = require('crypto');
+const qs = require('qs');
 require("dotenv").config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -9,17 +12,18 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
-const context = new TuyaContext({
-    baseUrl: process.env.tyacurl,
-    accessKey: process.env.tyackey,
-    secretKey: process.env.tysckey,
-});
 
-const tysd2dlsens_id = process.env.tysd2dlsens_id;
+// 튜야 API 기본 정보 설정
+const baseUrl = process.env.tyacurl; // 
+const accessKey = process.env.tyackey; // 
+const secretKey = process.env.tysckey; 
+const tysd2dlsens_id = process.env.tysd2dlsens_id
+const tysd2fgbt_id = process.env.tysd2fgbt_id;
 
 async function sdbgdlchkPr() {
     var rstimechk = await rltimestr();
     console.log("sdbg2dlchk start!:" + rstimechk);
+
     var dlsensst = await dolcstatechk(tysd2dlsens_id);
     let rststring = "";
     //let datestring = hour +":"+s minute;
@@ -42,22 +46,17 @@ async function sdbgdlchkPr() {
 }
 
 async function dolcstatechk(device_id) { //dlcs control - chk
-    const commands = await context.request({
-        path: `/v1.0/iot-03/devices/${device_id}/status`,
-        method: 'GET',
-        //body: {}
-    });
-    if (!commands.success) {
-        new Error();
-    }
-    //console.log("Execution result:",commands);
+    await getToken();
+    const device_rst = await callTyApi('GET', `/v1.0/iot-03/devices/${device_id}/status`, {}, {});
+    console.log(device_rst);
+
     var rststring = "";
-    if (commands.result[0].value == true) {
+    if (device_rst[0].value == true) {
         rststring = true;
-    } else if (commands.result[0].value == false) {
+    } else if (device_rst[0].value == false) {
         rststring = false;
     }
-    return rststring
+    return rststring;
 }
 
 
@@ -65,8 +64,8 @@ async function sctytimebkPr() {
     // 월요일과 수요일을 숫자로 매핑합니다 (0: 일요일, 1: 월요일, ..., 6: 토요일).
     const targetDaysOfWeek = [0, 1, 2, 3, 4, 5, 6]; //
     const hgDayWeek = ["일", "월", "화", "수", "목", "금", "토"]
-    const hour = 21;  // 24시간 형식  
-    const minute = 35;
+    const hour = 12;  // 24시간 형식  
+    const minute = 47;
 
     // 원하는 요일과 시간에 함수를 실행하도록 스케줄링합니다.
     const rule = new schedule.RecurrenceRule();
@@ -157,4 +156,101 @@ async function rltimestr() {
     return formattedDateTime;
 }
 
+
+let token = '';
+let tokenExpireTime = 0;
+
+function encryptStr(str, secret) {
+  return crypto.createHmac('sha256', secret).update(str, 'utf8').digest('hex').toUpperCase();
+}
+
+async function getToken() {
+  const timestamp = Date.now().toString();
+  const signUrl = '/v1.0/token?grant_type=1';
+  const contentHash = crypto.createHash('sha256').update('').digest('hex');
+  const stringToSign = ['GET', contentHash, '', signUrl].join('\n');
+  const signStr = accessKey + timestamp + stringToSign;
+  const sign = encryptStr(signStr, secretKey);
+  const headers = {
+    t: timestamp,
+    sign_method: 'HMAC-SHA256',
+    client_id: accessKey,
+    sign: sign,
+  };
+  const options = {
+    method: 'GET',
+    url: baseUrl + signUrl,
+    headers: headers,
+  };
+  try {
+    const response = await axios(options);
+    if (response.data.success) {
+      token = response.data.result.access_token;
+      tokenExpireTime = response.data.result.expire_time;
+      //console.log('Token obtained successfully:', token);
+    } else {
+      console.error('Token request failed:', response.data.msg);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function callTyApi(method, path, params, data) {
+    const timestamp = Date.now().toString();
+    const sortedParams = {};
+    Object.keys(params)
+      .sort()
+      .forEach((key) => (sortedParams[key] = params[key]));
+    const querystring = decodeURIComponent(qs.stringify(sortedParams));
+    const url = querystring ? `${path}?${querystring}` : path;
+  
+    const contentHash = crypto.createHash('sha256').update(JSON.stringify(data)).digest('hex');
+    const stringToSign = [method, contentHash, '', url].join('\n');
+    const signStr = accessKey + token + timestamp + stringToSign;
+    const sign = encryptStr(signStr, secretKey);
+    const headers = {
+      t: timestamp,
+      path: url,
+      client_id: accessKey,
+      sign: sign,
+      sign_method: 'HMAC-SHA256',
+      access_token: token,
+    };
+    const options = {
+      method: method,
+      url: baseUrl + url,
+      headers: headers,
+      data: data,
+    };
+    try {
+      const response = await axios(options);
+      if (response.data.success) {
+        return response.data.result;
+      } else {
+        console.error('API request failed:', response.data.msg);
+        if (response.data.code === 1010 || response.data.code === 1011) {
+          // 토큰 만료 또는 무효일 경우, 새로운 토큰 요청
+          console.log('Token expired or invalid, requesting a new one...');
+          await getToken();
+          // 재귀적으로 API 호출 함수 다시 실행
+          return callTyApi(method, path, params, data);
+        } else {
+          return null;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+}
+
+
 module.exports = { sctytimebkPr };
+
+
+
+
+
+
+  
